ae69b3f0ca3e716a10f154b2f632bf3738e27d70
merestrickland <mere.strickland@gmail.com>
2019-05-16 17:24:09 -0400

    broken now but getting there

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,6 +1,22 @@
 
 const initialState = {
-    trips: []
+    trips: [
+        {
+          "id": 2,
+          "name": "Mere <3 NY",
+          "user": {
+            "id": 2,
+            "first_name": "Meredith",
+            "last_name": "Strickland"
+          },
+          "location": {
+            "id": 2,
+            "name": "New York City",
+            "coordinates": "40.7128, -74.0060"
+          }
+        }
+      ],
+    user: ""
 }
 
 
